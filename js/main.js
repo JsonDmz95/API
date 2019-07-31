0341d62d4203a5b0d3c3be856cbdaf947cafc677
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-31 10:42:14 -0700

    Adding active animation

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// $(document).ready(function() {
+//   var tl = new TimelineMax({ repeat: -1 });
+//   tl.staggerTo(".circle", 0.8, { opacity: 1 }, 0.5);
+// });
--- js/tools.js
+++ js/tools.js
@@ -1,111 +1,115 @@
 $(window).on("load", function() {
+  $(".animated").each(function() {
+    var item = $(this);
+    var delay = $(this).data("delay");
 
-    $('.animated').each(function() {
-        var item = $(this)
-        var delay = $(this).data("delay")
+    if (delay == null) {
+      delay = 0;
+    }
 
-        if (delay == null) {
-            delay = 0;
-        }
-
-        if ($(this).data("scroll") == 0 || $(this).data("scroll") == null) {
-            setTimeout(function() {
-                item.addClass('in');
-            }, delay);
-        }
-
-    });
+    if ($(this).data("scroll") == 0 || $(this).data("scroll") == null) {
+      setTimeout(function() {
+        item.addClass("in");
+      }, delay);
+    }
+  });
 });
-
 
 $(window).scroll(function() {
-    $('.animated').each(function() {
-        var position = $(this).offset().top;
-        var delay = $(this).data("delay")
-        var item = $(this)
-        if (delay == null) {
-            delay = 0;
-        }
-        $.fn.scrollBottom = function() {
-            return $(document).height() - this.scrollTop() - this.height();
-        };
-        var topOfWindow = $(window).scrollTop();
-        var bottomOfWindow = $(window).scrollBottom();
+  $(".animated").each(function() {
+    var position = $(this).offset().top;
+    var delay = $(this).data("delay");
+    var item = $(this);
+    if (delay == null) {
+      delay = 0;
+    }
+    $.fn.scrollBottom = function() {
+      return $(document).height() - this.scrollTop() - this.height();
+    };
+    var topOfWindow = $(window).scrollTop();
+    var bottomOfWindow = $(window).scrollBottom();
 
-        if ($(this).data("scroll") != 0 || $(this).data("scroll") != null) {
-            if (position < topOfWindow + 700 || position == bottomOfWindow) {
-                setTimeout(function() {
-                    item.addClass('in');
-                }, delay);
-            }
-        }
-    });
+    if ($(this).data("scroll") != 0 || $(this).data("scroll") != null) {
+      if (position < topOfWindow + 700 || position == bottomOfWindow) {
+        setTimeout(function() {
+          item.addClass("in");
+        }, delay);
+      }
+    }
+  });
 });
-$('.scroll-to, .navbar-nav li a, .local-scroll').click(function(e) {
-    var jump = $(this).attr('href');
-    var position = $(jump).offset();
-    $('body, html').stop().animate({
+$(".scroll-to, .navbar-nav li a, .local-scroll").click(function(e) {
+  var jump = $(this).attr("href");
+  var position = $(jump).offset();
+  $("body, html")
+    .stop()
+    .animate(
+      {
         scrollTop: position.top
-    }, 1000);
-    e.preventDefault();
+      },
+      1000
+    );
+  e.preventDefault();
 });
 
 (function() {
-    $('.multiple-item .item').each(function() {
-        var itemToClone = $(this);
+  $(".multiple-item .item").each(function() {
+    var itemToClone = $(this);
 
-        for (var i = 1; i < 4; i++) {
-            itemToClone = itemToClone.next();
+    for (var i = 1; i < 4; i++) {
+      itemToClone = itemToClone.next();
 
-            // wrap around if at end of item collection
-            if (!itemToClone.length) {
-                itemToClone = $(this).siblings(':first');
-            }
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(":first");
+      }
 
-            // grab item, clone, add marker class, add to collection
-            itemToClone.children(':first-child').clone()
-            .addClass("cloneditem-" + (i))
-            .appendTo($(this));
-        }
-    });
+      // grab item, clone, add marker class, add to collection
+      itemToClone
+        .children(":first-child")
+        .clone()
+        .addClass("cloneditem-" + i)
+        .appendTo($(this));
+    }
+  });
 
-    $('.carousel-testimonial').each(function(){
-        $(this).carousel({interval: $(this).data("interval")}).on('slide.bs.carousel', function(e) {
-            var nextH = $(e.relatedTarget).height();
-            $(this).find('.item.active').parent().animate({
-                height: nextH
-            }, $(this).data("adjust"));
-        });
-    });
-
-}());
+  $(".carousel-testimonial").each(function() {
+    $(this)
+      .carousel({ interval: $(this).data("interval") })
+      .on("slide.bs.carousel", function(e) {
+        var nextH = $(e.relatedTarget).height();
+        $(this)
+          .find(".item.active")
+          .parent()
+          .animate(
+            {
+              height: nextH
+            },
+            $(this).data("adjust")
+          );
+      });
+  });
+})();
 
 $(document).ready(function() {
-
   $("input.form-control").on("keyup change", function() {
     // If empty
-    if($(this).val() == ""){
+    if ($(this).val() == "") {
       $(this).removeClass("with-value"); // Remove class
-    }
-
-    else{
+    } else {
       $(this).addClass("with-value"); // Add class
     }
   });
 
   $("textarea.form-control").on("keyup change", function() {
     // If empty
-    if($(this).val() == ""){
+    if ($(this).val() == "") {
       $(this).removeClass("with-value"); // Remove class
-    }
-
-    else{
+    } else {
       $(this).addClass("with-value"); // Add class
     }
   });
-
 });
-
 
 // $(window).load(function() {
 //   setTimeout(function() {
