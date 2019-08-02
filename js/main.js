e25e41e8df3ee76f960acee2a9308aee50697264
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-08-02 15:49:41 -0700

    Finishing drop truck

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-// $(document).ready(function() {
-//   var tl = new TimelineMax({ repeat: -1 });
-//   tl.staggerTo(".circle", 0.8, { opacity: 1 }, 0.5);
-// });
+$(document).ready(function() {
+  // Scroll target on Home Drivers section
+  $("#home_drivers").on("inview", function(event, isInView) {
+    if (isInView) {
+      new TimelineMax().to("#drop_truck_mobile", 1, { opacity: 1, y: 0 }, 0.1);
+      new TimelineMax().to("#drop_truck", 1.5, { opacity: 1, y: -500 }, 0.1);
+    } else {
+      new TimelineMax().to("#drop_truck_mobile", 1, { opacity: 0, y: -95 }, 0.1);
+      new TimelineMax().to("#drop_truck", 1.5, { opacity: 0, y: -650 }, 0.1);
+    }
+  });
+});
