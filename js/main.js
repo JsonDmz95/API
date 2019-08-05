4cd206fe3afff90f6e79f8d1eeabd435dcd5464e
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-08-05 10:22:08 -0700

    Adding animation

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
       new TimelineMax().to("#drop_truck_mobile", 1, { opacity: 1, y: 0 }, 0.1);
       new TimelineMax().to("#drop_truck", 1.5, { opacity: 1, y: -500 }, 0.1);
     } else {
-      new TimelineMax().to("#drop_truck_mobile", 1, { opacity: 0, y: -95 }, 0.1);
-      new TimelineMax().to("#drop_truck", 1.5, { opacity: 0, y: -650 }, 0.1);
+      // new TimelineMax().to("#drop_truck_mobile", 1, { opacity: 0, y: -95 }, 0.1);
+      // new TimelineMax().to("#drop_truck", 1.5, { opacity: 0, y: -650 }, 0.1);
     }
   });
 });
